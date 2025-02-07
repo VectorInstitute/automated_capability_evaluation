9d020252438c139f55cd6b99732daef0b1183b6b
xeon27 <omkar.dige@vectorinstitute.ai>
2025-02-06 19:05:07 -0500

    Create initial pipeline for task generation

--- src/run.py
+++ src/run.py
@@ -2,9 +2,9 @@ import hydra
 from omegaconf import DictConfig
 
 from model import Model
-from task import MathTask
+from task import Task
 
-from pprint import pprint
+from utils.prompts import TASK_GENERATION_SYSTEM_PROMPT, TASK_GENERATION_USER_PROMPT
 
 
 @hydra.main(version_base=None, config_path="cfg", config_name="run_cfg")
@@ -15,30 +15,38 @@ def main(cfg: DictConfig):
     Args:
         cfg (DictConfig): Configuration for the model.
     """
-    math_task = MathTask(
+    domain = "math"
+    math_task = Task(
         cfg.tasks.task_cfgs.mathematics,
     )
-    print(math_task)
+    gsm8k_task = Task(
+        cfg.tasks.task_cfgs.gsm8k,
+    )
 
-    # # Create an instance of the Model class with the specified model name
-    # model = Model(
-    #     model_name=cfg.model.name,
-    #     **cfg.prompt_cfg,
-    # )
+    # Set system message
+    sys_msg = TASK_GENERATION_SYSTEM_PROMPT
 
-    # # Define a sample input
-    # sample_input = "Are LLMs capable of generating evaluation benchmarks for self-evaluation?"
+    # Create an instance of the Model class with the specified model name
+    model = Model(
+        model_name=cfg.model.name,
+        sys_msg=sys_msg,
+    )
 
-    # # Generate output using the model with specified generation arguments
-    # gen_cfg = cfg.gen_cfg
-    # output, metadata = model.generate(
-    #     prompt=sample_input,
-    #     generation_config=gen_cfg,
-    # )
+    # Model input
+    sample_input = TASK_GENERATION_USER_PROMPT.format(prev_tasks="\n".join([str(math_task), str(gsm8k_task)]), domain=domain)
 
-    # # Print the output
-    # print(f"Output: {output}")
-    # print(f"Metadata: {metadata}")
+    # Generate output using the model with specified generation arguments
+    gen_cfg = cfg.gen_cfg
+    output, metadata = model.generate(
+        prompt=sample_input,
+        generation_config=gen_cfg,
+    )
+
+    # Print the output
+    print(f"Model: {model.get_model_name()}")
+    print(f"Output:\n\n{output}\n\n")
+    print(f"Metadata: {metadata}")
+
 
 if __name__ == "__main__":
     main()
