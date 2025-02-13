52445270669f3a69d5a174c71b696131f6ab0533
xeon27 <omkar.dige@vectorinstitute.ai>
2025-02-13 16:44:26 -0500

    Add src.

--- src/task.py
+++ src/task.py
@@ -3,8 +3,8 @@ from typing import Any, Dict, Union
 
 from datasets import Dataset
 
-from model import Model
-from utils import load_data
+from src.model import Model
+from src.utils import load_data
 
 
 class Task:
