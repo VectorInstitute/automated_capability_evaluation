dfbfaf8505a9c99970acef134a34292ea8084981
xeon27 <omkar.dige@vectorinstitute.ai>
2025-03-21 18:31:09 -0400

    Update with trial run option

--- src/run.py
+++ src/run.py
@@ -12,12 +12,15 @@ def main(cfg: DictConfig) -> None:
     Args:
         cfg (DictConfig): Configuration for the model.
     """
+    run_id = f"{cfg.generator_model.name}_{cfg.capabilities_cfg.num_gen_capabilities}"
     _ = generate_capabilities(
         domain=cfg.capabilities_cfg.domain,
         num_capabilities=cfg.capabilities_cfg.num_gen_capabilities,
         scientist_llm=cfg.generator_model.name,
         num_seed_capabilities=cfg.capabilities_cfg.num_seed_capabilities,
         scientist_llm_gen_cfg=cfg.generator_model.gen_cfg,
+        run_id=run_id,
+        trial_run=cfg.exp_cfg.trial_run,
     )
 
 
