8614615f664e31347cc94b7379356ae91059751c
xeon27 <omkar.dige@vectorinstitute.ai>
2025-03-27 18:55:02 -0400

    Comment out WIP code

--- src/run.py
+++ src/run.py
@@ -2,10 +2,13 @@ import hydra  # noqa: D100
 from omegaconf import DictConfig
 
 from generate_capabilities import filter_capabilities, generate_capabilities
-from generate_tasks import generate_tasks_using_llm
-from lbo import generate_new_capability, get_lbo_train_set
+
+# from generate_tasks import generate_tasks_using_llm
+# from lbo import generate_new_capability
 from model import Model
-from utils.prompts import TASK_GENERATION_SYSTEM_PROMPT, TASK_GENERATION_USER_PROMPT
+
+
+# from utils.lbo_utils import get_lbo_train_set
 
 
 def check_cfg(cfg: DictConfig) -> None:
@@ -63,75 +66,75 @@ def main(cfg: DictConfig) -> None:
     capabilities = filter_capabilities(capabilities)
     print(capabilities)
 
-    # Stage 2. Generate tasks and evaluate subject model on initial capabilities
-    num_lbo_runs = cfg.lbo_cfg.num_lbo_runs
-    if cfg.lbo_cfg.pipeline_id == "nearest_neighbour":
-        # For pipeline 1 (pipeline_id=="nearest_neighbour"), the set of
-        # generated capabilities are split into two sets
-        train_capabilities, candidate_capabilities = get_lbo_train_set(
-            input_data=capabilities,
-            train_frac=cfg.lbo_cfg.train_frac,
-            min_train_size=cfg.lbo_cfg.min_train_size,
-        )
-        if num_lbo_runs > len(candidate_capabilities):
-            print(
-                f"Warning: Number of LBO runs ({num_lbo_runs}) exceeds "
-                + f"the number of candidate capabilities ({len(candidate_capabilities)}). "
-                + f"Setting the number of LBO runs to {len(candidate_capabilities)}."
-            )
-            num_lbo_runs = len(candidate_capabilities)
-    elif cfg.lbo_cfg.pipeline_id == "discover_new":
-        # For pipeline 2 (pipeline_id=="discover_new"), use all generated capabilities
-        # for training
-        train_capabilities = capabilities
-        candidate_capabilities = None
+    # # Stage 2. Generate tasks and evaluate subject model on initial capabilities
+    # num_lbo_runs = cfg.lbo_cfg.num_lbo_runs
+    # if cfg.lbo_cfg.pipeline_id == "nearest_neighbour":
+    #     # For pipeline 1 (pipeline_id=="nearest_neighbour"), the set of
+    #     # generated capabilities are split into two sets
+    #     train_capabilities, candidate_capabilities = get_lbo_train_set(
+    #         input_data=capabilities,
+    #         train_frac=cfg.lbo_cfg.train_frac,
+    #         min_train_size=cfg.lbo_cfg.min_train_size,
+    #     )
+    #     if num_lbo_runs > len(candidate_capabilities):
+    #         print(
+    #             f"Warning: Number of LBO runs ({num_lbo_runs}) exceeds the number of "
+    #             + f"candidate capabilities ({len(candidate_capabilities)}). "
+    #             + f"Setting the number of LBO runs to {len(candidate_capabilities)}."
+    #         )
+    #         num_lbo_runs = len(candidate_capabilities)
+    # elif cfg.lbo_cfg.pipeline_id == "discover_new":
+    #     # For pipeline 2 (pipeline_id=="discover_new"), use all generated capabilities
+    #     # for training
+    #     train_capabilities = capabilities
+    #     candidate_capabilities = None
 
-    # Initialize the subject LLM model
-    subject_llm = Model(cfg.subject_llm.name)
+    # # Initialize the subject LLM model
+    # subject_llm = Model(cfg.subject_llm.name)
 
-    # TODO: Run this asynchronosly
-    for capability in capabilities:
-        # Generate tasks for each capability
-        generate_tasks_using_llm(
-            capability=capability,
-            scientist_llm=scientist_llm,
-            sys_prompt=TASK_GENERATION_SYSTEM_PROMPT,
-            user_prompt=TASK_GENERATION_USER_PROMPT,
-            num_tasks=cfg.capabilities_cfg.num_gen_tasks_per_capability,
-            scientist_llm_gen_cfg=cfg.scientist_llm.gen_cfg,
-        )
-        # Evaluate subject LLM on each capability
-        capability.evaluate([subject_llm])
+    # # TODO: Run this asynchronosly
+    # for capability in capabilities:
+    #     # Generate tasks for each capability
+    #     generate_tasks_using_llm(
+    #         capability=capability,
+    #         scientist_llm=scientist_llm,
+    #         sys_prompt=TASK_GENERATION_SYSTEM_PROMPT,
+    #         user_prompt=TASK_GENERATION_USER_PROMPT,
+    #         num_tasks=cfg.capabilities_cfg.num_gen_tasks_per_capability,
+    #         scientist_llm_gen_cfg=cfg.scientist_llm.gen_cfg,
+    #     )
+    #     # Evaluate subject LLM on each capability
+    #     capability.evaluate([subject_llm])
 
-    # Stage 3. Use LBO to generate new capabilities
-    for lbo_run_id in range(num_lbo_runs):
-        new_capability = generate_new_capability(
-            capabilities=train_capabilities,
-            subject_llm_name=cfg.subject_llm.name,
-            capabilities_pool=candidate_capabilities,
-            pipeline_id=cfg.lbo_cfg.pipeline_id,
-            lbo_run_id=lbo_run_id,
-        )
-        # Generate tasks for new capability
-        generate_tasks_using_llm(
-            capability=new_capability,
-            scientist_llm=scientist_llm,
-            sys_prompt=TASK_GENERATION_SYSTEM_PROMPT,
-            user_prompt=TASK_GENERATION_USER_PROMPT,
-            num_tasks=cfg.capabilities_cfg.num_gen_tasks_per_capability,
-            scientist_llm_gen_cfg=cfg.scientist_llm.gen_cfg,
-        )
-        # Evaluate subject LLM on new capability
-        new_capability.evaluate([subject_llm])
-        # Add new capability to train capabilities list
-        train_capabilities.append(new_capability)
-        # Remove new capability from candidate capabilities
-        # for pipeline 1
-        if candidate_capabilities is not None:
-            candidate_capabilities.remove(new_capability)
+    # # Stage 3. Use LBO to generate new capabilities
+    # for lbo_run_id in range(num_lbo_runs):
+    #     new_capability = generate_new_capability(
+    #         capabilities=train_capabilities,
+    #         subject_llm_name=cfg.subject_llm.name,
+    #         capabilities_pool=candidate_capabilities,
+    #         pipeline_id=cfg.lbo_cfg.pipeline_id,
+    #         lbo_run_id=lbo_run_id,
+    #     )
+    #     # Generate tasks for new capability
+    #     generate_tasks_using_llm(
+    #         capability=new_capability,
+    #         scientist_llm=scientist_llm,
+    #         sys_prompt=TASK_GENERATION_SYSTEM_PROMPT,
+    #         user_prompt=TASK_GENERATION_USER_PROMPT,
+    #         num_tasks=cfg.capabilities_cfg.num_gen_tasks_per_capability,
+    #         scientist_llm_gen_cfg=cfg.scientist_llm.gen_cfg,
+    #     )
+    #     # Evaluate subject LLM on new capability
+    #     new_capability.evaluate([subject_llm])
+    #     # Add new capability to train capabilities list
+    #     train_capabilities.append(new_capability)
+    #     # Remove new capability from candidate capabilities
+    #     # for pipeline 1
+    #     if candidate_capabilities is not None:
+    #         candidate_capabilities.remove(new_capability)
 
-    new_capabilities = train_capabilities[-num_lbo_runs:]
-    print(f"New capabilities: {new_capabilities}")
+    # new_capabilities = train_capabilities[-num_lbo_runs:]
+    # print(f"New capabilities: {new_capabilities}")
 
 
 if __name__ == "__main__":
